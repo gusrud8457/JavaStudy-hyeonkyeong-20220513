b1a00e8336816c1b67f1f56ffed099f045a2737d
gusrud8457 <fjqm7280@naver.com>
2022-05-16 21:49:29 +0900

    연산 및 스캐너

---
+++ javaStudy_hyeonkyeong/src/j03_연산/Operation01.java
@@ -0,0 +1,18 @@
+package j03_연산;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		num= num +1;
+		
+		System.out.println(num++);//++num : 선진가 , 후진가: 그 다음부터 증가시키기
+		System.out.println(num);
+		System.out.println(++num);
+		System.out.println(num);
+
+		
+		
+	}
+
+}
---
+++ javaStudy_hyeonkyeong/src/j03_연산/Operation05.java
@@ -0,0 +1,14 @@
+package j03_연산;
+
+public class Operation05 {
+
+	public static void main(String[] args) {
+		int num = 15;
+		num = num+2;
+		num+= 2;//복합대입연산자
+		num %= 3;
+		
+		System.out.println(num);
+	}
+
+}
